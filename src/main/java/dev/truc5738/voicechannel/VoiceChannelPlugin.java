2a26d368b36fdac57d4e5f301a71ac108e3e6380
Truc5738 <hyung0101978@gmail.com>
2026-09-23 13:15:37 +0700

    Integrate voice routing and Alpine-safe gateway

--- src/main/java/dev/truc5738/voicechannel/VoiceChannelPlugin.java
+++ src/main/java/dev/truc5738/voicechannel/VoiceChannelPlugin.java
@@ -5,17 +5,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class VoiceChannelPlugin extends JavaPlugin {
     private VoiceManager voiceManager;
     private VoiceMenu voiceMenu;
+    private VoiceGateway voiceGateway;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
 
         voiceManager = new VoiceManager(this);
+        voiceGateway = new VoiceGateway(this, voiceManager);
+        voiceGateway.start();
+
         voiceMenu = new VoiceMenu(this, voiceManager);
 
         VoiceCommand command = new VoiceCommand(voiceMenu);
-        getCommand("voicechannel").setExecutor(command);
-        getCommand("voicechannel").setTabCompleter(command);
+        if (getCommand("voicechannel") != null) {
+            getCommand("voicechannel").setExecutor(command);
+            getCommand("voicechannel").setTabCompleter(command);
+        }
 
         getServer().getPluginManager().registerEvents(voiceMenu, this);
         getLogger().info("Minecraft-VoiceChannel enabled.");
@@ -23,12 +29,15 @@ public final class VoiceChannelPlugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        if (voiceManager != null) {
-            voiceManager.shutdown();
-        }
+        if (voiceGateway != null) voiceGateway.stop();
+        if (voiceManager != null) voiceManager.shutdown();
     }
 
     public VoiceManager getVoiceManager() {
         return voiceManager;
+    }
+
+    public VoiceGateway getVoiceGateway() {
+        return voiceGateway;
     }
 }
