d0af93fd966ad80341149f1f4d795c990e3fe078
Truc5738 <hyung0101978@gmail.com>
2026-09-26 06:43:14 +0700

    Use published Paper 26.2 API build

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.26.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:26.2.build.129-stable")
     compileOnly("org.geysermc.floodgate:api:2.2.5-SNAPSHOT")
 }
 
