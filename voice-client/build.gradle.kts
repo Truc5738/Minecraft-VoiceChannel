7323b4ece7aff6b174d2637409725c5a83086a10
Truc5738 <hyung0101978@gmail.com>
2026-09-25 14:52:47 +0700

    Add Java voice client prototype

---
+++ voice-client/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    application
+}
+
+application {
+    mainClass.set("dev.truc5738.voiceclient.VoiceClient")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(25)
+    options.encoding = "UTF-8"
+}
+
+tasks.jar {
+    archiveBaseName.set("Minecraft-VoiceChannel-VoiceClient")
+    manifest.attributes["Main-Class"] = application.mainClass.get()
+}
